9baec069314f038a3cf30c1568ae0ec38ecd49c7
Eduardo Sganderla <eduardosganderla@MacBook-Pro-de-Eduardo.local>
2022-06-14 21:16:06 -0300

    Classe Especialidade, Client Especialidade

---
+++ src/model/especialidade.model.ts
@@ -0,0 +1,11 @@
+import { AbstractEntity } from "./abstract-entity.model";
+
+/**
+ * @author Eduardo Sganderla
+ *
+ * @since 1.0.0, 14/06/2022
+ * @version 1.0.0
+ */
+export class Especialidade extends AbstractEntity{
+    nome! : string
+}
---
+++ src/model/sexo.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Eduardo Sganderla
+ *
+ * @since 1.0.0, 14/06/2022
+ * @version 1.0.0
+ */
+export enum Sexo{
+    masculino = 'masculino',
+        feminino = 'feminino',
+            outro = 'outro'
+}
